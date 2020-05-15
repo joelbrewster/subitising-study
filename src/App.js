ae7b5fe425a405ef88f4ee99b47e682001d5229e
Joel Brewster <joel@iMac.hub>
2020-05-15 21:56:59 +1000

    Start of build

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const refreshPage = ()=>{
+        window.location.reload();
+    }
+
+    var numberarray = []
+    for (var i = 0, l = 10; i < l; i++) {
+        numberarray.push(Math.floor(Math.random() * 2))
+    }
+
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              See and say
+            </h1>
+          </header>
+          <div className="content">
+            <div className="row">
+
+              {numberarray.map(number =>
+                               <div className="dot-container">
+                                 <div className={"dot active-" + number}></div>
+                               </div>
+                              )}
+            </div>
+            <button type="submit" onClick={() => { refreshPage() }}>Click</button>
+          </div>
+        </div>
+    );
 }
 
 export default App;
